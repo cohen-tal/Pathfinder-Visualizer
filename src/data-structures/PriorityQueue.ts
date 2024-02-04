e104b0d51f134eb360b95e83f58292abb584ae98
Tal Cohen <tlcsbh@gmail.com>
2024-02-04 18:16:23 +0200

    refactor screen size breakpoints + refactor PriorityQueue methods return types

--- src/data-structures/PriorityQueue.ts
+++ src/data-structures/PriorityQueue.ts
@@ -3,30 +3,30 @@ import Node from "@/classes/node";
 export default class PriorityQueue {
   values: [number, Node][] = [];
 
-  enqueue(val: Node, priority: number) {
+  enqueue(val: Node, priority: number): void {
     this.values.push([priority, val]);
     this.sort();
   }
 
-  dequeue() {
+  dequeue(): [number, Node] | undefined {
     return this.values.shift();
   }
 
-  has(val: Node) {
+  has(val: Node): boolean {
     return this.values.some((v) => v[1] === val);
   }
 
-  changePriority(val: Node, priority: number) {
+  changePriority(val: Node, priority: number): void {
     const index = this.values.findIndex((v) => v[1] === val);
     this.values[index][0] = priority;
     this.sort();
   }
 
-  isEmpty() {
+  isEmpty() : boolean {
     return this.values.length === 0;
   }
 
-  sort() {
+  sort(): void {
     this.values.sort((a, b) => a[0] - b[0]);
   }
 }
