34cdcdf511dbf8aba44db3a5737ec96fc8bd230e
Tal Cohen <tlcsbh@gmail.com>
2024-02-05 09:49:26 +0200

    refactor binary-tree algorithm

--- src/utils/maze/binary-tree.ts
+++ src/utils/maze/binary-tree.ts
@@ -19,23 +19,30 @@ export default function binaryTreeMaze(graph: Graph): Set<Node> {
         }
     });
 
-    //make a path from bottom left to top right of the maze and the path is either right or up
-    for(let row = rows - 2; row > 0; row-=2) {
-        for(let col = 1; col < cols - 1; col+=2) {
+    for (let i = 1; i < rows - 1; i += 2) {
+        for (let j = 1; j < cols - 1; j += 2) {
+            const node = graph.nodes[i][j];
             const goRight = Math.random() > 0.5;
-            if(row === 1) {
-                path.push(graph.nodes[row][col + 1]);
+            if (goRight) {
+                const rightNeighbor = graph.nodes[i][j + 1];
+                rightNeighbor.wall = false;
+                walls.delete(rightNeighbor);
             } else {
-                goRight ? path.push(graph.nodes[row][col + 1]) : path.push(graph.nodes[row - 1][col]);
+                const bottomNeighbor = graph.nodes[i + 1][j];
+                bottomNeighbor.wall = false;
+                walls.delete(bottomNeighbor);
             }
         }
     }
-    
-    for (let i = cols - 2; i > 0; i--) {
-        const node = graph.nodes[rows - 2][i];
-        if (!path.includes(node)) {
-            walls.delete(node);
-            node.wall = false;
+
+    const flip: boolean = Math.random() > 0.5;
+    if (flip) {
+        for (let i = cols - 2; i > 0; i--) {
+            path.push(graph.nodes[rows - 2][i]);
+        }
+    } else {
+        for (let i = cols - 2; i > 0; i--) {
+            path.push(graph.nodes[1][i]);
         }
     }
 
