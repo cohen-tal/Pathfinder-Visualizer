b6f7f139e3b5568afe43b75c8a7abe25dd4efc48
Tal Cohen <cohental63@gmail.com>
2024-02-07 10:28:58 +0200

    add reset graph drop down menu

---
+++ src/app/components/dropdown-menu/ResetDropDownMenu.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { ClickAwayListener } from "@mui/material";
+import DropDownMenu, { DropDownMenuProps } from "../menu/DropDownMenu";
+import MenuItem from "../menu/MenuItem";
+
+export default function ResetDropDownMenu({
+  menuName,
+  children,
+  resetAll,
+  resetWalls,
+  resetWeights,
+}: DropDownMenuProps & { resetAll: () => void, resetWalls: () => void, resetWeights: () => void }) {
+  return (
+    <DropDownMenu menuName={menuName}>
+      <MenuItem onClick={resetAll}>Reset All</MenuItem>
+      <MenuItem onClick={resetWalls}>Reset Walls</MenuItem>
+      <MenuItem onClick={resetWeights}>Reset Weights</MenuItem>
+    </DropDownMenu>
+  );
+}
