2e567d63313108a658602fca16dd87da97f1b387
Tal Cohen <cohental63@gmail.com>
2024-02-10 17:24:06 +0200

    refactor component

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -1,14 +1,19 @@
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 export default function Footer() {
   return (
     <div className="absolute bottom-0 w-full flex flex-row items-center justify-center gap-2  border-slate-900/10 dark:border-slate-300/10 dark:bg-slate-700 border-t">
-        <p className="font-semibold text-sm p-1">Made with ❤️ by Tal Cohen</p>
-        <>
-        <a className="pl-2" href='https://github.com/cohen-tal' target="_blank"><GitHubIcon sx={{fontSize:"2rem"}} /></a>
-        <a className="pl-2" href='https://www.linkedin.com/in/tal-cohen-89ba68210/' target="_blank"><LinkedInIcon fontSize="large" /></a>
-        </>
+      <a className="pl-2" href="https://github.com/cohen-tal" target="_blank">
+        <GitHubIcon sx={{ fontSize: "2rem" }} />
+      </a>
+      <a
+        className="pl-2"
+        href="https://www.linkedin.com/in/tal-cohen-89ba68210/"
+        target="_blank"
+      >
+        <LinkedInIcon fontSize="large" />
+      </a>
     </div>
-  )
+  );
 }
