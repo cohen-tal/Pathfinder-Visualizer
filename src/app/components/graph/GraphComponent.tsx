69ed876ef22d206ee92fe82fef3ae47190e821bc
Tal Cohen <cohental63@gmail.com>
2024-02-13 16:22:37 +0200

    refactor code so component is now pure

--- src/app/components/graph/GraphComponent.tsx
+++ src/app/components/graph/GraphComponent.tsx
@@ -11,14 +11,14 @@ import {
 
 export interface GraphProps {
   graphNodes: Node[][];
-  children?: React.ReactNode;
+  onWallChange?: (id: [number, number], wall: boolean) => void;
+  onWeightChange?: (id: [number, number], weight: number) => void;
   visitedNodes?: Set<Node>;
   shortestPath?: Node[];
 }
 
 export default function GraphComponent({
   graphNodes,
-  children,
   visitedNodes,
   shortestPath,
 }: GraphProps) {
@@ -52,47 +52,36 @@ export default function GraphComponent({
     });
   }
 
-  useEffect(() => {
-    if (visitedNodes) {
-      const visitedNodeAnimations: Promise<void>[] = animate(
-        visitedNodes,
-        styles.visited
-      );
-
-      Promise.all(visitedNodeAnimations).then(() => {
-        setTimeout(() => {
-          if (shortestPath) {
-            animateShortestPath(shortestPath, styles.shortestPath);
-          }
-        }, 1000);
-      });
-    }
-  }, [visitedNodes, shortestPath]);
+  // useEffect(() => {
+  //   setNodes(graphNodes);
+  // }, [graphNodes]);
 
   return (
-    <div
-      className={`grid grid-cols-[repeat(64,1fr)] grid-rows-[repeat(21 ,1fr)] hover:cursor-pointer`}
-    >
-      {nodes.map((row) => {
-        return row.map((node) => {
-          return (
-            <NodeComponent
-              key={`${node.position[0]}-${node.position[1]}`}
-              id={node.position}
-              weight={node.weight}
-              isStartNode={node.start}
-              isEndNode={node.end}
-              isWall={node.wall}
-              changeWall={(id: [number, number], wall: boolean) => {
-                handleWallChange(id, wall);
-              }}
-              changeWeight={(id: [number, number], weight: number) => {
-                handleWeightChange(id, weight);
-              }}
-            />
-          );
-        });
-      })}
+    <div className="border-slate-900/10 dark:border-slate-300/10 rounded-[7px] border-[1px] shadow-lg">
+      <div
+        className={`grid grid-cols-[repeat(17,1fr)] grid-rows-[repeat(31,1fr)] z-[-1] md:grid-cols-[repeat(63,1fr)] md:grid-rows-[repeat(21,1fr)] hover:cursor-pointer`}
+      >
+        {graphNodes.map((row) => {
+          return row.map((node) => {
+            return (
+              <NodeComponent
+                key={`${node.position[0]}-${node.position[1]}`}
+                id={node.position}
+                weight={node.weight}
+                isStartNode={node.start}
+                isEndNode={node.end}
+                isWall={node.wall}
+                changeWall={(id: [number, number], wall: boolean) => {
+                  handleWallChange(id, wall);
+                }}
+                changeWeight={(id: [number, number], weight: number) => {
+                  handleWeightChange(id, weight);
+                }}
+              />
+            );
+          });
+        })}
+      </div>
     </div>
   );
 }
